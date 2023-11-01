0f242a7500b67830ddf2f85d95b70a3a623f683e
strongmhk <kim6562166086@gmail.com>
2023-11-01 16:52:22 +0900

    django pinterest course

---
+++ python/prac/pragmatic/accountapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from accountapp.views import hello_world
+
+app_name = "accountapp"  # "127.0.0.1:8000/account/hello_world"을 호출할 필요없이 "accountapp:hello_world"로 치환해줌
+
+
+urlpatterns = [
+    path("hello_world/", hello_world, name="hello_world")
+]
