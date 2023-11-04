d6f0f09144e0ce3fbcbe3bd9b3eda1139bcb3b7f
strongmhk <kim6562166086@gmail.com>
2023-11-04 23:23:05 +0900

    django course 21 commit

--- python/prac/pragmatic/accountapp/urls.py
+++ python/prac/pragmatic/accountapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"  # "127.0.0.1:8000/account/hello_world"을 호출할 필요없이 "accountapp:hello_world"로 치환해줌
 
 
 urlpatterns = [
-    path("hello_world/", hello_world, name="hello_world")
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- python/prac/pragmatic/accountapp/views.py
+++ python/prac/pragmatic/accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -19,5 +22,14 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+
+class AccountCreateView(CreateView):
+    model = User # 장고가 User 모델을 기본으로 제공, 이것을 상속받아 새로운 필드 추가 가능
+    form_class = UserCreationForm # 회원가입을 할 때 사용할 form
+    success_url = reverse_lazy('accountapp:hello_world') # 계정을 만들기에 성공했을 때, 어느 경로로 다시 연결할 것인지
+    # 함수형 뷰에서는 reverse만을, class형 뷰에서는 reverse_lazy만 사용할 수 있다
+    template_name = 'accountapp/create.html' # 회원가입시에 렌더링해줄 html
 
 
