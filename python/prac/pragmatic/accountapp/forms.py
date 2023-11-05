f13162f0dca943882cf0b9cccf23ec44bfee5c7c
strongmhk <kim6562166086@gmail.com>
2023-11-05 09:42:41 +0900

    django course 26 commit

--- python/prac/pragmatic/accountapp/forms.py
+++ python/prac/pragmatic/accountapp/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class AccountUpdateForm(UserCreationForm):
-    def __int__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields['username'].disabled = True
--- python/prac/pragmatic/accountapp/urls.py
+++ python/prac/pragmatic/accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"  # "127.0.0.1:8000/account/hello_world"을 호출할 필요없이 "accountapp:hello_world"로 치환해줌
 
@@ -15,4 +15,6 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
+
 ]
