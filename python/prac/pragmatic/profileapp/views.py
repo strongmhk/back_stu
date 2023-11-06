14881d00a03097c21b88342d771a1ed85dd0a327
strongmhk <kim6562166086@gmail.com>
2023-11-06 20:22:18 +0900

    django course 33 commit

--- python/prac/pragmatic/profileapp/views.py
+++ python/prac/pragmatic/profileapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -15,7 +15,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form): # ProfileCreationForm으로 부터 전송된 image, nickname 필드 등이 form이라는 변수에 저장됨
@@ -25,6 +24,9 @@ class ProfileCreateView(CreateView):
 
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) # 여기서 self.object는 model로 설정한 Profile 객체를 의미함
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -32,5 +34,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) # 여기서 self.object는 model로 설정한 Profile 객체를 의미함
