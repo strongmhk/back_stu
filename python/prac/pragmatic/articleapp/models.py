7aadacdb61e31b96fc11261944b9c7f558d279f5
strongmhk <kim6562166086@gmail.com>
2023-11-07 16:42:08 +0900

    django course 36 commit

--- python/prac/pragmatic/articleapp/models.py
+++ python/prac/pragmatic/articleapp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) # related_name 옵션은 user.article과 같이 역참조가 가능하게 하기 위해서이다
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True) # 긴 글 일수도 있으므로 Text 필드 사용
+    created_at = models.DateField(auto_now_add=True, null=True)
