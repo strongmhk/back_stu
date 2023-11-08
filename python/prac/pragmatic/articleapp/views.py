eae174145703a90ac96fe905db93dd10d053dee9
strongmhk <kim6562166086@gmail.com>
2023-11-08 14:38:31 +0900

    django course 37 commit

--- python/prac/pragmatic/articleapp/views.py
+++ python/prac/pragmatic/articleapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -55,3 +55,11 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+
+class ArticleListView(ListView):
+    model = Article # 어떤 모델을 사용하는지
+    context_object_name = 'article_list' # 해당 템플릿에서 Article 모델을 어떤 이름으로 사용할건지
+    template_name = 'articleapp/list.html' # 템플릿 이름
+    paginate_by = 25 # 한 페이지에 몇 개의 객체를 보여줄 것인지
