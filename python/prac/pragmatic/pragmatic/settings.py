d2602ceae747b70b1a75d9eb6ae50a9eadabd997
strongmhk <kim6562166086@gmail.com>
2023-11-11 11:45:05 +0900

    django course 40 commit

--- python/prac/pragmatic/pragmatic/settings.py
+++ python/prac/pragmatic/pragmatic/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # 모든 호스트에 대해서 허용
 
 
 # Application definition
