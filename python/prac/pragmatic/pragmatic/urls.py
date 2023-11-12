1fb49d3780403f1d62c7309378296a6e191a4a3f
strongmhk <kim6562166086@gmail.com>
2023-11-12 11:36:06 +0900

    django course 43 commit

--- python/prac/pragmatic/pragmatic/urls.py
+++ python/prac/pragmatic/pragmatic/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("articles/", include('articleapp.urls')),
     path("comments/", include('commentapp.urls')),
     path("projects/", include('projectapp.urls')),
+    path("subscribes/", include('subscribeapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
