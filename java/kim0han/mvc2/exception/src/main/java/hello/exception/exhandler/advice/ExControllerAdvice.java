473a0c7b3ffcaf1b6cb9f0bf76942cc71b5a44ea
strongmhk <kim6562166086@gmail.com>
2023-11-26 01:06:56 +0900

    MVC2 Ch9-8

---
+++ java/kim0han/mvc2/exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,49 @@
+package hello.exception.exhandler.advice;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "hello.exception.api")
+public class ExControllerAdvice {
+
+    /*    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String illegalExHandler(IllegalArgumentException e){
+        log.error("[exceptionHandler] ex", e);
+        return "error/500";
+    }
+    @Controller 사용 후 return값을 view이름으로 해주면
+    Controller에서 ViewResolver를 사용했던 것처럼
+    뷰를 응답하는 것도 가능!
+    */
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e){
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e){
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e){
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+
+}
