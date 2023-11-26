76da3514380454bd7ca1ce1e97fd08376eb88faa
strongmhk <kim6562166086@gmail.com>
2023-11-27 00:33:50 +0900

    MVC2 Ch10-5,6

---
+++ java/kim0han/mvc2/typeconverter/src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,26 @@
+package hello.typeconverter.formatter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter formatter = new MyNumberFormatter();
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L); // Long 타입 주의, Number 타입(추상클래스)의 구현체인 Long 타입(자식)으로 해야됨
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
