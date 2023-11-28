ce4bb46c0c300bf3f23ee1f0607823fbb96e4e5d
strongmhk <kim6562166086@gmail.com>
2023-11-28 15:01:00 +0900

    DB1 Ch1-5

---
+++ java/kim0han/db1/jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst { // 객체 생성못하도록 추상 클래스로 생성
+    public static final String URL = "jdbc:h2:tcp://localhost/~/Student";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+
+}
