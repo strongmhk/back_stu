6517d309c3af7f52e80fe89c1592e8651d01a3ef
strongmhk <kim6562166086@gmail.com>
2023-12-05 17:31:47 +0900

    DB1 Ch6-5
    스프링 예외 추상화 적용

---
+++ java/kim0han/db1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
@@ -0,0 +1,147 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.ex.MyDbException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * SQLExceptionTranslator 추가
+ */
+@Slf4j
+public class MemberRepositoryV4_2 implements MemberRepository {
+
+    private final DataSource dataSource;
+    private final SQLExceptionTranslator exTranslator;
+
+    public MemberRepositoryV4_2(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+//        "insert into member(member_id, money) values (insert into ..., select a from ...)"
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId()); // ?에 들어갈 파라미터 바인딩
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate(); // 쿼리가 DB에 실행
+            return member;
+        } catch (SQLException e) {
+            throw exTranslator.translate("save", sql, e);
+        } finally { // 커넥션을 끊기 위함
+            close(con, pstmt, null);
+        }
+
+
+    }
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();// select는 executeUpdate가 아니고 executeQuery사용
+            if (rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else { // 데이터가 없음
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+        } catch (SQLException e){
+            throw exTranslator.translate("findById", sql, e);
+        } finally {
+            close(con, pstmt, rs);
+        }
+
+
+    }
+
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money); // ?에 들어갈 파라미터 바인딩
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();// 쿼리가 DB에 실행
+            log.info("resultSize={}",resultSize); // 0 이나 1이 나옴
+        } catch (SQLException e) {
+            throw exTranslator.translate("update", sql, e);
+        } finally { // 커넥션을 끊기 위함
+            close(con, pstmt, null);
+        }
+    }
+
+    @Override
+    public void delete(String memberId) {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId); // ?에 들어갈 파라미터 바인딩
+            int resultSize = pstmt.executeUpdate();// 쿼리가 DB에 실행
+            log.info("resultSize={}",resultSize); // 0 이나 1이 나옴
+        } catch (SQLException e) {
+            throw exTranslator.translate("delete", sql, e);
+        } finally { // 커넥션을 끊기 위함
+            close(con, pstmt, null);
+        }
+
+
+    }
+
+
+    private void close(Connection con, Statement stmt, ResultSet rs){ // Statement는 PreparedStatement의 부모
+
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        // 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야한다.
+        DataSourceUtils.releaseConnection(con, dataSource);
+
+    }
+
+    private Connection getConnection() throws SQLException {
+        // 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야한다.
+        Connection con = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection={}, class={}", con, con.getClass());
+        return con;
+    }
+}
