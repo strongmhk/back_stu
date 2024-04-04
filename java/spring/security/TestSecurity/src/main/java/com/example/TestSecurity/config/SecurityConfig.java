7e977d7b41f5bf53a349f8a3688dbac2d42a7261
strongmhk <kim6562166086@gmail.com>
2024-04-04 21:11:59 +0900

    feat: formLogin 비활성화, httpBasic 활성화

--- java/spring/security/TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ java/spring/security/TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.TestSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -30,11 +31,13 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
-        http.
-                formLogin((auth) -> auth.loginPage("/login")
-                        .loginProcessingUrl("/loginProc")
-                        .permitAll()
-                );
+//        http.
+//                formLogin((auth) -> auth.loginPage("/login")
+//                        .loginProcessingUrl("/loginProc")
+//                        .permitAll()
+//                );
+        http
+                .httpBasic(Customizer.withDefaults());
 
         http
                 .csrf((auth) -> auth.disable());
