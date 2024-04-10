8bce1d2fa71b131dc05e38d978e3fa745f24eaeb
strongmhk <kim6562166086@gmail.com>
2024-04-10 16:06:14 +0900

    feat:

---
+++ java/spring/security/SpringJWT/src/main/java/com/example/SpringJWT/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.SpringJWT.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+
+        return "main Controller";
+    }
+}
