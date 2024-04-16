e0eeba2d7f4cbb442b9ab14f1470950d404d326c
strongmhk <kim6562166086@gmail.com>
2024-04-16 20:56:10 +0900

    test: 전체 가격 계산 테스트
    - @DisplayName으로 이름을 섬세하게
    - Given, When, Then 템플릿

--- java/spring/tdd/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ java/spring/tdd/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.unit;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
@@ -22,6 +23,10 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+
+
+//    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     @Test
     void add() throws Exception {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -77,6 +82,24 @@ class CafeKioskTest {
 
         cafeKiosk.clear();
         assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
+    @Test
+    void calculateTotalPrice() throws Exception {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+
+        // when
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        // then
+        assertThat(totalPrice).isEqualTo(8500);
     }
 
     @Test
