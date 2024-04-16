64fde90425eaa08c412996e6bd17c8c8df9207e7
strongmhk <kim6562166086@gmail.com>
2024-04-16 20:55:21 +0900

    refactor: 전체 가격 계산 메서드
    stream 사용

--- java/spring/tdd/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ java/spring/tdd/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -40,11 +40,9 @@ public class CafeKiosk {
     }
 
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-        for (Beverage beverage : beverages) {
-            totalPrice += beverage.getPrice();
-        }
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 
     public Order createOrder() {
@@ -57,6 +55,7 @@ public class CafeKiosk {
         return new Order(currentDateTime, beverages);
     }
 
+    // 테스트하기 위해 통제가 어려운 변수를 외부로 분리
     public Order createOrder(LocalDateTime currentDateTime) {
         LocalTime currentTime = currentDateTime.toLocalTime();
         if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
