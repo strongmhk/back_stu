34f890be3975b71ba19c0ef6f27ced23adcc8586
strongmhk <kim6562166086@gmail.com>
2024-09-15 21:08:38 +0900

    init: 프로젝트 세팅 및 SecurityConfig 생성

---
+++ java/spring/security/OAuthJWT/src/test/java/com/example/OAuthJWT/OAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuthJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
