016d1ddcf74635933885d95fd2c4d47179a3bf7c
strongmhk <kim6562166086@gmail.com>
2024-09-15 23:56:23 +0900

    :sparkles: feat: JWT 필터 구현 및 등록

--- java/spring/security/OAuthJWT/src/main/java/com/example/OAuthJWT/config/SecurityConfig.java
+++ java/spring/security/OAuthJWT/src/main/java/com/example/OAuthJWT/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.OAuthJWT.config;
 
+import com.example.OAuthJWT.jwt.JWTFilter;
 import com.example.OAuthJWT.jwt.JWTUtil;
 import com.example.OAuthJWT.oauth2.CustomSuccessHandler;
 import com.example.OAuthJWT.service.CustomOAuth2UserService;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +36,10 @@ public class SecurityConfig {
         //HTTP Basic 인증 방식 disable
         http
                 .httpBasic((auth) -> auth.disable());
+
+        //JWTFilter 추가
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         //oauth2
         http
